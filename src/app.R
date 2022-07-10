5b369a6a2e15ba4fa468cbf4e42a825ad7eec67b
Jorrit Vander Mynsbrugge <44178217+jorritvm@users.noreply.github.com>
2022-07-10 12:06:55 +0200

    prefixed libraries

--- src/app.R
+++ src/app.R
@@ -3,21 +3,21 @@ source("R/libraries.R")
 source("R/ui.R") # also sources the ui subcomponets
 source("R/server.R")
 
-source("R/bolero.R")
-source("R/currency.R")
-source("R/portfolio_plots.R")
-source("R/portfolio_positions.R")
-source("R/portfolio_transactions.R")
-source("R/saxo.R")
-source("R/stock_ohlc.R")
-source("R/stock_plots.R")
-source("R/stock_profiles.R")
-source("R/utils.R")
+source("R/lib_bolero.R")
+source("R/lib_currency.R")
+source("R/lib_portfolio_plots.R")
+source("R/lib_portfolio_positions.R")
+source("R/lib_portfolio_transactions.R")
+source("R/lib_saxo.R")
+source("R/lib_stock_ohlc.R")
+source("R/lib_stock_plots.R")
+source("R/lib_stock_profiles.R")
+source("R/lib_utils.R")
 
 options(shiny.maxRequestSize = 30 * 1024 ^ 2) # 30 MB
 reactlog_enable()
 
 options = list(host = "0.0.0.0", port = 9999)
-ui <- secure_app(ui, enable_admin = FALSE)
+ui <- secure_app(ui, enable_admin = FALSE) # activate when running live
 shinyApp(ui = ui, server = server, options = options)
 
